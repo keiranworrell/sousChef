de59d21ab61322d77baa8dd547b5ac601db30e33
Keiran Worrell <worrellkeiran@gmail.com>
2026-06-19 11:56:25 +0000

    fix(terraform): add required format argument to API Gateway access_log_settings

--- infrastructure/terraform/modules/api_gateway/main.tf
+++ infrastructure/terraform/modules/api_gateway/main.tf
@@ -27,6 +27,17 @@ resource "aws_apigatewayv2_stage" "default" {
 
   access_log_settings {
     destination_arn = aws_cloudwatch_log_group.access_logs.arn
+    format = jsonencode({
+      requestId      = "$context.requestId"
+      ip             = "$context.identity.sourceIp"
+      requestTime    = "$context.requestTime"
+      httpMethod     = "$context.httpMethod"
+      routeKey       = "$context.routeKey"
+      status         = "$context.status"
+      protocol       = "$context.protocol"
+      responseLength = "$context.responseLength"
+      errorMessage   = "$context.error.message"
+    })
   }
 
   tags = {
