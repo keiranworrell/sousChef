83d1749dbd1714ca759d65c97602a1bbccf033a8
Keiran Worrell <worrellkeiran@gmail.com>
2026-06-19 12:37:17 +0000

    fix(terraform): use execution_arn for recipes Lambda permission source_arn

--- infrastructure/terraform/modules/api_gateway/outputs.tf
+++ infrastructure/terraform/modules/api_gateway/outputs.tf
@@ -7,3 +7,8 @@ output "api_endpoint" {
   description = "The default invoke URL for the API (includes https://)"
   value       = aws_apigatewayv2_stage.default.invoke_url
 }
+
+output "execution_arn" {
+  description = "Execution ARN used for Lambda permission source_arn (arn:aws:execute-api:...)"
+  value       = aws_apigatewayv2_api.this.execution_arn
+}
